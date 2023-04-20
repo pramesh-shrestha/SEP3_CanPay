142b22b701592425dcdb3b8c44f2f569a42a8551
Pramesh Shrestha <prmsstha22@gmail.com>
2023-04-20 20:57:52 +0200

    T2 - Added: UserService (Defined methods on the interface)

---
+++ SEP3_AppTier_DataTier/ApplicationTier/src/main/java/service/UserService.java
@@ -0,0 +1,14 @@
+package service;
+
+import entity.User;
+
+import java.util.List;
+
+public interface UserService {
+  User createUser(User user);
+  List<User> fetchUsers();
+  User fetchUserById(Long id);
+  User fetchUserByUsername(String username);
+  User updateUser(User user);
+  void deleteUser(Long id);
+}
