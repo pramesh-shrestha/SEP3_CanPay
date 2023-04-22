b667f2552cda3012952e21ba49e857bf40e91394
Pramesh Shrestha <prmsstha22@gmail.com>
2023-04-22 23:32:50 +0200

    T3: Added - UserDaoImpl.cs and also added fetchUserById in IUserDao.cs

--- SEP3_DataTier/EFCDataAccess/DAOInterface/IUserDao.cs
+++ SEP3_DataTier/EFCDataAccess/DAOInterface/IUserDao.cs
@@ -5,8 +5,9 @@ namespace EFCDataAccess.DAOInterface;
 public interface IUserDao {
     Task<User> CreateUserAsync(User user);
     Task<User> FetchUserByUsernameAsync(string username);
+    Task<User> FetchUserByIdAsync(long id);
     Task<ICollection<User>> FetchUsersAsync();
     Task<User> UpdateUserAsync(User user);
-    Task DeleteUserAsync(int id);
+    Task DeleteUserAsync(long id);
 
 }
