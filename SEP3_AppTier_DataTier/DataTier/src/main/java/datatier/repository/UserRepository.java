755646a3017a73c726ec93a18af2d79a5cd212f9
Rajib Paudyal <82520479+rajipkanxo01@users.noreply.github.com>
2023-04-22 17:33:47 +0200

    Delete SEP3_AppTier_DataTier/DataTier directory

--- SEP3_AppTier_DataTier/DataTier/src/main/java/datatier/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package datatier.repository;
-
-import datatier.entity.UserEntity;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface UserRepository extends JpaRepository<UserEntity,Long>{
-}
