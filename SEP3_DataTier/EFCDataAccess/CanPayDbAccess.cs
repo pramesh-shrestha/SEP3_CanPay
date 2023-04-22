51939a3a25bbdd8421161f6d7ad63b28c0b5a251
Pramesh Shrestha <prmsstha22@gmail.com>
2023-04-22 22:50:02 +0200

    T3: Added - CanPayDbAccess DbContext and configured for postgresql

---
+++ SEP3_DataTier/EFCDataAccess/CanPayDbAccess.cs
@@ -0,0 +1,16 @@
+﻿using Entity.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCDataAccess; 
+
+public class CanPayDbAccess : DbContext {
+    public DbSet<User> Users { get; set; }
+    public DbSet<DebitCard> Cards { get; set; }
+
+    protected CanPayDbAccess(DbContextOptions<CanPayDbAccess> options) : base(options) {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        modelBuilder.UseSerialColumns();
+    }
+}
