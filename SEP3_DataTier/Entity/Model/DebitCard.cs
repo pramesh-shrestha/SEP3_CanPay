74c7affd04c0db72e9f7ea13bcef7d6a05bd3e72
Pramesh Shrestha <prmsstha22@gmail.com>
2023-04-22 22:49:17 +0200

    T3: Added - annotations and constraints

--- SEP3_DataTier/Entity/Model/DebitCard.cs
+++ SEP3_DataTier/Entity/Model/DebitCard.cs
@@ -5,7 +5,11 @@ namespace Entity.Model;
 public class DebitCard {
     [Key]
     public long CardId { get; set; }
+    [Required]
+    [StringLength(16, ErrorMessage = "The card number must contain 16 digits")]
     public long CardNumber { get; set; }
+    [Required]
     public string ExpiryDate { get; set; }
+    [Required]
     public int CVV { get; set; }
 }
--- SEP3_DataTier/Entity/Model/User.cs
+++ SEP3_DataTier/Entity/Model/User.cs
@@ -1,12 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Entity.Model; 
 
 public class User {
     [Key]
     public string Username { get; set; }
-
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required]
+    [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^\w\d\s:])([^\s]){8,30}$")]
     public string Password { get; set; }
+    [Required]
     public string Fullname { get; set; }
     public DebitCard Card { get; set; }
 }
