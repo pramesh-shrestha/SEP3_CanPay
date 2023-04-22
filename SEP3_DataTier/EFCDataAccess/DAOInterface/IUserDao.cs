87b1064f090e33e4a59c7244d70c6b3fd5c2f082
Pramesh Shrestha <prmsstha22@gmail.com>
2023-04-22 22:04:07 +0200

    T3 - Added: IUserDao.cs

---
+++ SEP3_DataTier/EFCDataAccess/DAOInterface/IUserDao.cs
@@ -0,0 +1,12 @@
+﻿using Entity.Model;
+
+namespace EFCDataAccess.DAOInterface; 
+
+public interface IUserDao {
+    Task<User> CreateUserAsync(User user);
+    Task<User> FetchUserByUsernameAsync(string username);
+    Task<ICollection<User>> FetchUsersAsync();
+    Task<User> UpdateUserAsync(User user);
+    Task DeleteUserAsync(int id);
+
+}
