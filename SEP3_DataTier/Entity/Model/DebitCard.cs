27f1d750d270c64bc42d6b47e38ba9983ec3e000
Pramesh Shrestha <prmsstha22@gmail.com>
2023-04-22 21:11:57 +0200

    T3: Added - User and DebitCard entities

---
+++ SEP3_DataTier/Entity/Model/DebitCard.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entity.Model; 
+
+public class DebitCard {
+    [Key]
+    public long CardId { get; set; }
+    public long CardNumber { get; set; }
+    public string ExpiryDate { get; set; }
+    public int CVV { get; set; }
+}
---
+++ SEP3_DataTier/Entity/Model/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entity.Model; 
+
+public class User {
+    [Key]
+    public string Username { get; set; }
+
+    public string Password { get; set; }
+    public string Fullname { get; set; }
+    public DebitCard Card { get; set; }
+}
