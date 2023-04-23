4d4def3c105f97a54ed664ba5300a0bdeb96209e
iamsu <iamsuhani5@gmail.com>
2023-04-23 16:18:11 +0200

    T3- Card dao implementation

---
+++ SEP3_DataTier/EFCDataAccess/DAOImplementation/CardDaoImpl.cs
@@ -0,0 +1,65 @@
+﻿using EFCDataAccess.DAOInterface;
+using Entity.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace EFCDataAccess.DAOImplementation;
+
+public class CardDaoImpl : ICardDao
+{
+
+    private readonly CanPayDbAccess context;
+
+    public CardDaoImpl(CanPayDbAccess context)
+    {
+        this.context = context;
+    }
+    
+
+    //method to create Card
+    public async Task<DebitCardEntity> CreateCardAsync(DebitCardEntity debitCardEntity)
+    {
+        try
+        {
+            EntityEntry<DebitCardEntity> cardAdded = await context.Cards.AddAsync(debitCardEntity);
+            await context.SaveChangesAsync();
+            return cardAdded.Entity;
+        }
+        catch (Exception e)
+        {
+            throw new Exception("Card already exists");
+        }
+    }
+
+    //Fetch card by username
+    public async Task<DebitCardEntity> FetchCardByUsernameAsync(string username)
+    {
+        DebitCardEntity? debitCard = await context.Cards.FindAsync(username);
+        if (debitCard==null)
+        {
+            throw new Exception("Card doesnt exist");
+        }
+        return debitCard;
+    }
+
+    //update card 
+    public async Task<DebitCardEntity> UpdateCardAsync(DebitCardEntity debitCardEntity)
+    {
+        context.Cards.Update(debitCardEntity);
+        await context.SaveChangesAsync();
+        return debitCardEntity;
+    }
+
+    //delete cardd
+    public async Task DeleteCardAsync(long id)
+    {
+        DebitCardEntity? existedCardEntity = await context.Cards.FirstOrDefaultAsync(card => card.CardId == id);
+        if (existedCardEntity==null)
+        {
+            throw new Exception($"Card with id {id} doesnt exists");
+        }
+
+        context.Cards.Remove(existedCardEntity);
+        await context.SaveChangesAsync();
+    }
+}
---
+++ SEP3_DataTier/EFCDataAccess/DAOInterface/ICardDao.cs
@@ -0,0 +1,12 @@
+﻿using Entity.Model;
+
+namespace EFCDataAccess.DAOInterface;
+
+public interface ICardDao
+{
+
+    Task<DebitCardEntity> CreateCardAsync(DebitCardEntity debitCardEntity);
+    Task<DebitCardEntity> FetchCardByUsernameAsync(string username);
+    Task<DebitCardEntity> UpdateCardAsync(DebitCardEntity debitCardEntity);
+    Task DeleteCardAsync(long id);
+}
