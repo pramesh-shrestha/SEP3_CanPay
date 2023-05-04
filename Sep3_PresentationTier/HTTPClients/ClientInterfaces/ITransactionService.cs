71aa7e2846b753fc31c8b3e52abbc9668d67c432
Pramesh Shrestha <prmsstha22@gmail.com>
2023-05-04 21:16:38 +0200

    T1: implemented CreateTransactionAsync in TransactionService

---
+++ Sep3_PresentationTier/HTTPClients/ClientInterfaces/ITransactionService.cs
@@ -0,0 +1,13 @@
+﻿using Domains.Entity;
+
+namespace HTTPClients.ClientInterfaces; 
+
+public interface ITransactionService {
+    Task<TransactionEntity> CreateTransactionAsync(TransactionEntity transactionEntity);
+    Task<TransactionEntity> FetchTransactionByIdAsync(long id);
+    Task<TransactionEntity> FetchAllTransactionBySender(string username);
+    Task<TransactionEntity> FetchAllTransactionByReceiver(string username);
+    Task<TransactionEntity> FetchAllTransactionInvolvingBothUsers(string username);
+    
+    
+}
---
+++ Sep3_PresentationTier/HTTPClients/Implementations/TransactionService.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using Domains.Entity;
+using HTTPClients.ClientInterfaces;
+
+namespace HTTPClients.Implementations;
+
+public class TransactionService : ITransactionService {
+
+    private HttpClient client;
+
+    public TransactionService(HttpClient client) {
+        this.client = client;
+    }
+
+    public async Task<TransactionEntity> CreateTransactionAsync(TransactionEntity transactionEntity) {
+        HttpResponseMessage response = await client.PatchAsJsonAsync("/transaction/create", transactionEntity);
+        string result = await response.Content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode) {
+            throw new Exception(result);
+        }
+
+        TransactionEntity entity = JsonSerializer.Deserialize<TransactionEntity>(result,
+            new JsonSerializerOptions {
+                PropertyNameCaseInsensitive = true
+            })!;
+        return entity;
+    }
+
+    public Task<TransactionEntity> FetchTransactionByIdAsync(long id) {
+        throw new NotImplementedException();
+    }
+
+    public Task<TransactionEntity> FetchAllTransactionBySender(string username) {
+        throw new NotImplementedException();
+    }
+
+    public Task<TransactionEntity> FetchAllTransactionByReceiver(string username) {
+        throw new NotImplementedException();
+    }
+
+    public Task<TransactionEntity> FetchAllTransactionInvolvingBothUsers(string username) {
+        throw new NotImplementedException();
+    }
+}
