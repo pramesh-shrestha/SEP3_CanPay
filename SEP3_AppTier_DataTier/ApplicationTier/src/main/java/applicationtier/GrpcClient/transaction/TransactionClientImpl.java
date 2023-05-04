445d2fa0ebb8e9a4eabdf3f43be6bf94eec38b57
iamsu <iamsuhani5@gmail.com>
2023-05-04 10:06:34 +0200

    T2: Conversion from proto to entity and vice-versa

--- SEP3_AppTier_DataTier/ApplicationTier/src/main/java/applicationtier/GrpcClient/transaction/TransactionClientImpl.java
+++ SEP3_AppTier_DataTier/ApplicationTier/src/main/java/applicationtier/GrpcClient/transaction/TransactionClientImpl.java
@@ -60,6 +60,9 @@ public class TransactionClientImpl implements ITransactionClient{
         return null;
     }
 
+
+
+
     @Override
     public List<TransactionEntity> fetchAllTransactionInvolvingUser(String username) {
         return null;
