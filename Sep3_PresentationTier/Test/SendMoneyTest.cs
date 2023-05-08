98770fc5a3066d1129e432887cd388885d8164b8
saransing <325611@via.dk>
2023-05-08 10:31:05 +0200

    T1: - Added SendMoney Test class

---
+++ Sep3_PresentationTier/Test/SendMoneyTest.cs
@@ -0,0 +1,107 @@
+﻿using BlazorServerApp.Pages;
+using Bunit;
+using HTTPClients.ClientInterfaces;
+using HTTPClients.Implementations;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace Test {
+
+    public class TransactionTest : IClassFixture<TestContext> {
+
+        private TestContext context;
+        private IRenderedComponent<SendMoney> renderedComponent;
+        private Mock<ITransactionService> transactionService;
+
+        private void Setup()
+        {
+            transactionService = new Mock<ITransactionService>();
+            var client = new HttpClient();
+            var userService = new Mock<IUserService>();
+            context = new TestContext();
+            context.Services.AddSingleton<ITransactionService>(transactionService.Object);
+            context.Services.AddSingleton<IUserService>(userService.Object);
+            context.Services.AddSingleton<ICardService>(new CardService(client));
+            // renderedComponent = context.RenderComponent<SendMoney>(parameters => parameters.AddSingleton<ITransactionService>(transactionService.Object));
+            renderedComponent = context.RenderComponent<SendMoney>();
+        }
+
+        private void SetInstancesValue()
+        {
+            renderedComponent.Instance.sender = "Saran";
+            renderedComponent.Instance.receiver = "Kevin";
+            renderedComponent.Instance.amount = "100";
+        }
+
+
+        // [Fact] public async Task SendMoneyAsync_ShouldThrowAnErrorMessage_WhenSenderNameFieldIsEmpty()
+        // {
+        //     //Arrange
+        //     Setup();
+        //     SetInstancesValue();
+        //     renderedComponent.Instance.sender = "";
+        //
+        //     //Act
+        //     // await renderedComponent.Instance.GetErrorMessage();
+        //
+        //     //Assert
+        //     Assert.Equal("Error: Sender name cannot be empty", renderedComponent.Instance.errorLabel);
+        // }
+
+        [Fact] public async Task SendMoneyAsync_ShouldThrowAnErrorMessage_WhenRecipientNameFieldIsEmpty()
+        {
+            //Arrange   
+            Setup();
+            SetInstancesValue();
+            renderedComponent.Instance.receiver = "";
+
+            //Act
+            await renderedComponent.Instance.GetErrorMessage();
+
+            //Assert
+            Assert.Equal("Receiver must be selected.", renderedComponent.Instance.errorLabel);
+        }
+
+        [Fact] public async Task SendMoneyAsync_ShouldThrowAnErrorMessage_WhenAmountFieldIsZero()
+        {
+            //Arrange   
+            Setup();
+            SetInstancesValue();
+            renderedComponent.Instance.amount = "0";
+
+            //Act
+            await renderedComponent.Instance.GetErrorMessage();
+
+            //Assert
+            Assert.Equal("Amount must be more than 0.", renderedComponent.Instance.errorLabel);
+        }
+
+        [Fact] public async Task SendMoneyAsync_ShouldThrowAnErrorMessage_WhenAmountIsNotANumber()
+        {
+            //Arrange   
+            Setup();
+            SetInstancesValue();
+            renderedComponent.Instance.amount = "&";
+        
+            //Act
+            await renderedComponent.Instance.GetErrorMessage();
+        
+            //Assert
+            Assert.Equal("Error: Amount must be a valid number", renderedComponent.Instance.errorLabel);
+        }
+
+        [Fact] public async Task SendMoneyAsync_ShouldThrowAnErrorMessage_WhenAmountIsNegative()
+        {
+            //Arrange   
+            Setup();
+            SetInstancesValue();
+            renderedComponent.Instance.amount = "-100";
+
+            //Act
+            await renderedComponent.Instance.GetErrorMessage();
+
+            //Assert
+            Assert.Equal("Error: Amount must be positive", renderedComponent.Instance.errorLabel);
+        }
+    }
+}
