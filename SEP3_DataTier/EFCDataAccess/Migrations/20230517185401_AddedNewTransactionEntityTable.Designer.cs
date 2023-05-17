501b05466cc89fa04b4798caaecb5a65e4544453
Pramesh Shrestha <prmsstha22@gmail.com>
2023-05-17 20:55:42 +0200

    T3: Made new Migration for Transaction entity

--- SEP3_DataTier/EFCDataAccess/Migrations/20230517185401_AddedNewTransactionEntityTable.Designer.cs
+++ SEP3_DataTier/EFCDataAccess/Migrations/20230517185401_AddedNewTransactionEntityTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EFCDataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EFCDataAccess.Migrations
 {
     [DbContext(typeof(CanPayDbAccess))]
-    partial class CanPayDbAccessModelSnapshot : ModelSnapshot
+    [Migration("20230517185401_AddedNewTransactionEntityTable")]
+    partial class AddedNewTransactionEntityTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SEP3_DataTier/EFCDataAccess/Migrations/20230517185401_AddedNewTransactionEntityTable.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCDataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNewTransactionEntityTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Comment",
+                schema: "CanPay",
+                table: "Transactions",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comment",
+                schema: "CanPay",
+                table: "Transactions");
+        }
+    }
+}
