e310e0ac41ec52299bc828ee2f494e28b2805ed6
iamsu <iamsuhani5@gmail.com>
2023-05-19 14:57:03 +0200

    T2 - generated proto file for bill payments

--- SEP3_AppTier_DataTier/ApplicationTier/src/main/java/applicationtier/service/serviceImplementations/RequestServiceImplementation.java
+++ SEP3_AppTier_DataTier/ApplicationTier/src/main/java/applicationtier/service/serviceImplementations/RequestServiceImplementation.java
@@ -1,6 +1,8 @@
 package applicationtier.service.serviceImplementations;
 
+import applicationtier.GrpcClient.notification.INotificationClient;
 import applicationtier.GrpcClient.request.IRequestClient;
+import applicationtier.entity.NotificationEntity;
 import applicationtier.entity.RequestEntity;
 import applicationtier.service.serviceInterfaces.IRequestService;
 import org.springframework.stereotype.Service;
@@ -11,16 +13,18 @@ import java.util.List;
 public class RequestServiceImplementation implements IRequestService {
 
     private IRequestClient requestClient;
+    private INotificationClient notificationClient;
 
-    public RequestServiceImplementation(IRequestClient requestClient) {
+    public RequestServiceImplementation(IRequestClient requestClient, INotificationClient notificationClient) {
         this.requestClient = requestClient;
+        this.notificationClient = notificationClient;
     }
 
     @Override
     public RequestEntity createRequest(RequestEntity requestEntity) {
         try {
             return requestClient.createRequest(requestEntity);
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
@@ -29,16 +33,16 @@ public class RequestServiceImplementation implements IRequestService {
     public List<RequestEntity> fetchAllRequest() {
         try {
             return requestClient.FetchAllRequest();
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
     }
 
     @Override
     public RequestEntity fetchRequestById(Long id) {
-        try{
+        try {
             return requestClient.FetchRequestById(id);
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
     }
@@ -47,7 +51,7 @@ public class RequestServiceImplementation implements IRequestService {
     public RequestEntity fetchRequestByUsername(String username) {
         try {
             return requestClient.FetchRequestByUsername(username);
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
     }
@@ -56,7 +60,7 @@ public class RequestServiceImplementation implements IRequestService {
     public RequestEntity updateRequest(RequestEntity requestEntity) {
         try {
             return requestClient.UpdateRequest(requestEntity);
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
     }
@@ -65,10 +69,8 @@ public class RequestServiceImplementation implements IRequestService {
     public boolean deleteRequest(Long id) {
         try {
             return requestClient.DeleteRequest(id);
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
     }
-
-
 }
